aee48ac70d2fea6f5359375f8f4fc0f93dad463f
Noe <noe.chaumette@gmail.com>
2024-03-19 10:01:11 +0100

    bb

--- bandit.py
+++ bandit.py
@@ -2,36 +2,46 @@ from random import randint, random
 from math import sqrt, log
 import matplotlib.pyplot as plt
 
-
+K = 2
+N = 100000
+cumulative_reward = [0 for i in range(10*100)]
+p = 0
+list_display = [x*N/10 for x in range(1, 11)]
 
 def pull(arm):
     arms = [0.4, 0.8]
     return arms[arm]
 
-def plot_cumulative_reward(name, N, cumulative_reward, offset):
+def mean_cumulative_reward(cumlative_reward):
+    average = [0 for i in range(10)]
+    for i in range(10):
+        average[i] = sum(cumulative_reward[i+x] for x in range(0,100,10))/10
+    return average
+
+def plot_cumulative_reward(name,N, cumulative_reward, list_display):
     plt.ylabel("Cumulative reward")
     plt.xlabel("Number of pulls")
     match name:
         case "random":
-            plt.plot(range(offset, N+offset), cumulative_reward)
+            plt.plot(list_display, cumulative_reward, "-r^")
         case "eps_greedy":
-            plt.plot(range(offset, N + offset), cumulative_reward)
+            plt.plot(list_display, cumulative_reward, "-go")
         case "eps_greedy_dec":
-            plt.plot(range(offset, N + offset), cumulative_reward)
+            plt.plot(list_display, cumulative_reward, "-yv")
         case "UCB":
-            plt.plot(range(offset, N + offset), cumulative_reward)
+            plt.plot(list_display, cumulative_reward, "-bs")
             
             
 def bandit(K,N,name):
     s = [0 for i in range(K)]
     n = [1 for i in range(K)]
-    cumulative_reward = [0 for i in range(N-K+1)]
-    p = 0
+    global cumulative_reward
+    global p
     for i in range(K):
         r = pull(i)
         s[i] = r
-    cumulative_reward[p] = sum(s[m] for m in range(K))
-    p += 1
+    #cumulative_reward[p] = sum(s[m] for m in range(K))
+    #p += 1
     for t in range(K+1,N+1):
         match name:
             case "random":
@@ -68,17 +78,32 @@ def bandit(K,N,name):
         r = pull(im)
         s[im] += r
         n[im] += 1
+        if(t in list_display):
+            cumulative_reward[p] = sum(s[m] for m in range(K)) 
+            p += 1      
         #for the plot
-        cumulative_reward[p] = cumulative_reward[p-1] + r
-        p += 1
-    plot_cumulative_reward(name, N-K+1 ,cumulative_reward, K)
+        #cumulative_reward[p] = cumulative_reward[p-1] + r
+        #p += 1
+    
     return sum(s[m] for m in range(K))
 
-K = 2
-N = 100000
 
-print(int(bandit(K,N,"random")))
-print(int(bandit(K,N,"eps_greedy")))
-print(int(bandit(K,N,"eps_greedy_dec")))
-print(int(bandit(K,N, "UCB")))
-#plt.show()
+
+#print(int(bandit(K,N,"random")))
+for i in range(100):
+    bandit(K,N,"random")
+plot_cumulative_reward("random", N ,mean_cumulative_reward(cumulative_reward), list_display)
+p = 0
+for i in range(100):
+    bandit(K,N,"eps_greedy")
+plot_cumulative_reward("eps_greedy", N ,mean_cumulative_reward(cumulative_reward), list_display)
+p = 0
+for i in range(100):
+    bandit(K,N,"eps_greedy_dec")
+plot_cumulative_reward("eps_greedy_dec", N ,mean_cumulative_reward(cumulative_reward), list_display)
+p = 0
+for i in range(100):
+    bandit(K,N,"UCB")
+plot_cumulative_reward("UCB", N ,mean_cumulative_reward(cumulative_reward), list_display)
+
+plt.show()
