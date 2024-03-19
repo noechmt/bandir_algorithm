609ee13179e07fcfc84e86f4bccbf46e2c38db34
Noe <noe.chaumette@gmail.com>
2024-03-19 08:54:26 +0100

    rewrote without numpy --> quicker

--- bandit.py
+++ bandit.py
@@ -1,11 +1,12 @@
+from random import randint, random
 import numpy as np
-import math
+from math import sqrt, log
 import matplotlib.pyplot as plt
 
 
 
 def pull(arm):
-    arms = [0.6, 0.8]
+    arms = [0.4, 0.8]
     return arms[arm]
 
 def plot_cumulative_reward(name, N, cumulative_reward, offset):
@@ -23,9 +24,9 @@ def plot_cumulative_reward(name, N, cumulative_reward, offset):
             
             
 def bandit(K,N,name):
-    s = np.zeros(K)
-    n = np.ones(K)
-    cumulative_reward = np.zeros(N-K+1)
+    s = [0 for i in range(K)]
+    n = [1 for i in range(K)]
+    cumulative_reward = [0 for i in range(N-K+1)]
     p = 0
     for i in range(K):
         r = pull(i)
@@ -35,27 +36,36 @@ def bandit(K,N,name):
     for t in range(K+1,N+1):
         match name:
             case "random":
-                im = np.random.randint(K)
+                im = randint(0, K-1)
             case "eps_greedy":
                 eps = 0.1
-                if np.random.random() < eps:
-                    im = np.random.randint(K)
+                if random() < eps:
+                    im = randint(0, K-1)
                 else:
-                    im = np.argmax(s/n)
+                    max = 0
+                    for i in range(K):
+                        temp = s[i]/n[i]
+                        if temp > max:
+                            max = temp
+                            im = i
             case "eps_greedy_dec":
-                eps = math.log(1/t**2)
-                if np.random.random() < eps:
-                    im = np.random.randint(K)
+                eps = 1/log(t**2)
+                if random() < eps:
+                    im = randint(0, K-1)
                 else:
-                    im = np.argmax(s/n)
+                    max = 0
+                    for i in range(K):
+                        temp = s[i]/n[i]
+                        if temp > max:
+                            max = temp
+                            im = i
             case "UCB":
                 max = 0
                 for i in range(K):
-                    temp = s[i]/n[i] + math.sqrt((2*math.log(t) / n[i]))
+                    temp = s[i]/n[i] + sqrt((2*log(t) / n[i]))
                     if temp > max:
                         max = temp
-                        indice = i
-                im = indice
+                        im = i
         r = pull(im)
         s[im] += r
         n[im] += 1
@@ -64,9 +74,9 @@ def bandit(K,N,name):
         p += 1
     plot_cumulative_reward(name, N-K+1 ,cumulative_reward, K)
     return sum(s[m] for m in range(K))
-        
+
 print(int(bandit(2,100000,"random")))
 print(int(bandit(2,100000,"eps_greedy")))
 print(int(bandit(2,100000,"eps_greedy_dec")))
 print(int(bandit(2, 100000, "UCB")))
-plt.show()
+#plt.show()
